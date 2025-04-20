bcca26986b454fe3eeb208913e32d6df8d7b1e7b
kaiokaize-sha <kaiokaizen.sha@gmail.com>
2025-04-20 23:35:42 +0530

    project setup

---
+++ src/main/java/com/dev/OpsVault/OpsVaultApplication.java
@@ -0,0 +1,13 @@
+package com.dev.OpsVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpsVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/OpsVault/OpsVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.OpsVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
